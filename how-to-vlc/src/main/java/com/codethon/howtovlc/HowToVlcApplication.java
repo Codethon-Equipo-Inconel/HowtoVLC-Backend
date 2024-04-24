72049865eac28acff3be812a8a4a988a836cd101
AmmielRarak <ammielary@gmail.com>
2024-04-24 19:04:43 +0200

    se agrego backend

---
+++ how-to-vlc/src/main/java/com/codethon/howtovlc/HowToVlcApplication.java
@@ -0,0 +1,13 @@
+package com.codethon.howtovlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToVlcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToVlcApplication.class, args);
+	}
+
+}
---
+++ how-to-vlc/src/test/java/com/codethon/howtovlc/HowToVlcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codethon.howtovlc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToVlcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
